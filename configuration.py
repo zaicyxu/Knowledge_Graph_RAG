2ba4eced6c1c6efbda01896a4e79e8d71fbbeb55
Hersmion Rain <106503993+zaicyxu@users.noreply.github.com>
2025-09-16 11:05:23 +0200

    Update configuration.py

--- configuration.py
+++ configuration.py
@@ -10,28 +10,10 @@
 GEMINI_API_KEY = "AIzaSyBdTPqt4RpQvOc676Z1v_OuEkDsqhrJd9k"
 
 # Neo4j Database Credentials
-NEO4J_URI = "neo4j+s://7451746a.databases.neo4j.io"
-NEO4J_USERNAME = "neo4j"
-NEO4J_PASSWORD = "gEGvMdOmiJh0bySsQe9Q5jZtyDbfiFZQcQUEf-T3ROM"
-NEO4J_DATABASE = "neo4j"
+NEO4J_URI = "bolt://localhost:7687"
+NEO4J_BLOOM_URI = "http://localhost:7474/bloom"
+NEO4J_USER = "neo4j"
+NEO4J_PASSWORD = "12345678"
 
 
-# Key entity types and relationships
-KEY_ENTITIES = {
-    "manufacturer": ["Work_on", "Certify"],
-    "material": ["Process"],
-    "industry": ["Belong", "Sub_Industry"],
-    "certification": ["Certify"],
-    "energy": ["Power_from"]
-}
 
-RELATIONSHIP_MAPPING = {
-    "Belong": "Industry",
-    "Certify": "Certification",
-    "Process": "Material",
-    "Work_on": "Service",
-    "Sub_Business": "Service",
-    "Sub_Industry": "Industry",
-    "Power_from": "Power"
-
-}
